8ee28e1552cbe6801aff35567a09afa9ce664dc1
charlie cox <charlie.cox@ibm.com>
2017-04-10 14:48:34 -0500

    going to cause SQ to fail

--- app.js
+++ app.js
@@ -23,7 +23,9 @@ var appEnv = cfenv.getAppEnv();
 
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
-
 	// print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
+  console.log("1" + appEnv.url);
+  console.log("2" + appEnv.url);
+  console.log("3" + appEnv.url);
 });
