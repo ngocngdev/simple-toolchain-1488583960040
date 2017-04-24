82a7740cf187d3322b07e6106fce6d8dc7b99085
charlie cox <charlie.cox@ibm.com>
2017-04-24 15:25:43 -0500

    added vulnerabilities

--- app.js
+++ app.js
@@ -24,5 +24,12 @@ var appEnv = cfenv.getAppEnv();
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
 	// print a message when the server starts listening
-
+	console.log("vulnerability");
+	console.log("vulnerability");
+	console.log("vulnerability");
+	console.log("vulnerability");
+	console.log("vulnerability");
+	console.log("vulnerability");
+	console.log("vulnerability");
+	console.log("vulnerability");
 });
